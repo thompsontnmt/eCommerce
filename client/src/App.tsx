ee3ce56c5414751123019b288caea260b4d454d9
Mike Thompson <mthompson@LAPTOP-OSM-MTHOMPSON.local>
2023-03-10 09:40:34 -0600

    fetch data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
   const [products, setProducts] =useState([
@@ -6,8 +6,14 @@ function App() {
       {name: 'product2', price: 200.00},
     ]);
 
+  useEffect(() => {
+    fetch('http://localhost:5000/api/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+  }, [])
+  
   function addProduct() {
-    setProducts([...products, {name: 'product3', price: 300.00}])
+    setProducts(prevState => [...prevState, {name: 'product' + (prevState.length + 1), price: (prevState.length * 100) + 100}])
   }
 
   return (
